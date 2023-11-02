d4eb0fd61aa4c90808f6dd87843606e829786fd8
aman <amanvyas055@gmail.com>
2023-11-02 13:14:45 +0530

    turtlesim_control

--- build/catkin_generated/setup_cached.sh
+++ build/catkin_generated/setup_cached.sh
@@ -8,7 +8,10 @@
 
 # modified environment variables
 export CMAKE_PREFIX_PATH="/home/aman/catkin_ws/src/turtlesim_control/build/devel:$CMAKE_PREFIX_PATH"
+export LD_LIBRARY_PATH="/home/aman/catkin_ws/src/turtlesim_control/build/devel/lib:$LD_LIBRARY_PATH"
 export PATH='/opt/ros/noetic/bin:/home/aman/.local/bin:/opt/ThirdParty-9/platforms/linux64Gcc/gperftools-svn/bin:/opt/paraviewopenfoam56/bin:/opt/ThirdParty-9/platforms/linux64Gcc/cmake-*/bin:/home/aman/OpenFOAM/aman-9/platforms/linux64GccDPInt32Opt/bin:/opt/site/9/platforms/linux64GccDPInt32Opt/bin:/opt/openfoam9/platforms/linux64GccDPInt32Opt/bin:/opt/openfoam9/bin:/opt/openfoam9/wmake:/home/aman/.local/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin'
+export PKG_CONFIG_PATH="/home/aman/catkin_ws/src/turtlesim_control/build/devel/lib/pkgconfig:$PKG_CONFIG_PATH"
 export PWD='/home/aman/catkin_ws/src/turtlesim_control/build'
+export PYTHONPATH="/home/aman/catkin_ws/src/turtlesim_control/build/devel/lib/python3/dist-packages:$PYTHONPATH"
 export ROSLISP_PACKAGE_DIRECTORIES="/home/aman/catkin_ws/src/turtlesim_control/build/devel/share/common-lisp:$ROSLISP_PACKAGE_DIRECTORIES"
 export ROS_PACKAGE_PATH="/home/aman/catkin_ws/src/turtlesim_control:$ROS_PACKAGE_PATH"
